225b520a45a1674de087a282544e270e24efc445
Motasim <makkimotasim@gmail.com>
2021-09-20 18:09:31 +0530

    feat: Added construtor

--- DSA/1.1 Arrays/3. Range Queries/implementation_of_square_root_decomposition.cpp
+++ DSA/1.1 Arrays/3. Range Queries/implementation_of_square_root_decomposition.cpp
@@ -1,11 +1,32 @@
 #include<iostream>
+#include<vector>
+#include<cmath>
 using namespace std;
 
 class SqrtDecomposition{
+    vector<int> arr;
+    vector<long> blockSum;
+    int sqroot;
 
+    public:
+        SqrtDecomposition(vector<int>& input){
+            sqroot = ceil(sqrt(input.size()));
+            arr.resize(sqroot*sqroot);
+            for(int i = 0; i < input.size(); i++)
+                arr[i] = input[i];
+            blockSum.resize(sqroot);
+            for(int i = 0; i < blockSum.size(); i++){
+                int startIndex = i*sqroot;
+                for(int j = 0; j < sqroot; j++)
+                    blockSum[i] += arr[startIndex+j];
+            }
+        }
 };
 
 int main(){
+
+    vector<int> input{1, 2, 6, 7, 9, 3, 1, 9};
+    SqrtDecomposition sqrtDecomposition(input);
 
     return 0;
 }
