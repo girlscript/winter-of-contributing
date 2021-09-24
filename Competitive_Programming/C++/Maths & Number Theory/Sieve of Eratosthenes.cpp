e785acd4df0af48a58fab6256c0d4e64e680b29a
Rishit Pandey <77775000+prishit55@users.noreply.github.com>
2021-09-24 22:11:55 +0530

    Update Sieve of Eratosthenes.cpp

--- Competitive_Programming/C++/Maths & Number Theory/Sieve of Eratosthenes.cpp
+++ Competitive_Programming/C++/Maths & Number Theory/Sieve of Eratosthenes.cpp
@@ -1,29 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
 void sieve(int n){
-    vector<bool>prime(n+1,true);       //boolean type vector to keep track of prime numbers, true for prime, false for composite
-    prime[0]=false;                    //0 is neither prime nor composite
-    prime[1]=false;                    //1 is composite
-    for(int i=2; i<=n; i++){                       //iterating from 2 to n
-        if(prime[i] && (long long)i*i<=n){         //if i'th number is prime then mark all its multiples as false(composite)
+    //boolean type vector to keep track of prime numbers, true for prime, false for composite
+    vector<bool>prime(n+1,true);       
+    prime[0]=false;                    
+    prime[1]=false;                   
+    for(int i=2; i<=n; i++){    
+        //if i'th number is prime then mark all its multiples as false(composite)
+        if(prime[i] && (long long)i*i<=n){         
             for(int j=i*i; j<=n; j+=i){
                 prime[j]=false;
             }
         }
     }
-    for(int i=0;i<n+1;i++){                     //print the primes
+    //print the primes
+    for(int i=0;i<n+1;i++){                     
         if(prime[i]){
             cout<<i<<" ";
         }
-    }                                      //Time Complexity:O(n*log(log(n)))
-}                                          //Space Complexity:O(n)
+    }                                      
+}                                          
 int main(){
-    int t;                                 //number of test cases
+    //number of test cases
+    int t;                                
     cin>>t;
     while(t--){
         int n;
-        cin>>n;                           //we have to find prime numbers till "n"
-        sieve(n);                         //function call
+        cin>>n;
+        //we have to find prime numbers till "n"
+        sieve(n);                         
         cout<<"\n";
     }
     return 0;
