9540372105d0746921072435cdd4c1a0e2a9809e
Pranay Sanjule <67793499+PranaySanjule@users.noreply.github.com>
2021-09-26 16:42:47 +0530

    Update prefixEvaluation.cpp

--- C_CPP/Data Structures/Stack/prefixEvaluation.cpp
+++ C_CPP/Data Structures/Stack/prefixEvaluation.cpp
@@ -1,1 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+class prefixEvaluation
+{
+
+public:
+    void prefEval(string s)
+    {
+        stack<int> st;                              //st stack to store the ans and perform the all operations.
+        for (int i = s.length() - 1; i >= 0; --i)   
+        {
+            if (s[i] >= '0' and s[i] <= '9')
+            {
+                st.push(s[i] - '0');
+            }
+            else
+            {
+                int first_op = st.top();
+                st.pop();
+                int second_op = st.top();
+                st.pop();
+
+                switch (s[i])
+                {
+                case '+':
+                    st.push(first_op + second_op);
+                    break;
+                case '-':
+                    st.push(first_op - second_op);
+                    break;
+                case '*':
+                    st.push(first_op * second_op);
+                    break;
+                case '/':
+                    st.push(first_op / second_op);
+                    break;
+                case '^':
+                st.push(pow(first_op,second_op));
+                break;
+            default:
+                exit(0);
+                break;
+                }
+            }
+        }
+        cout<<st.top();
+    }
+};
+
+int main()
+{   prefixEvaluation obj;
+
+    string s;                                           // Time Complexity => O(l), l = length of the string
+    cout << "Enter the prefix expression : " << endl;
+    cin >> s;
+    obj.prefEval(s);
+
+    return 0;
+}
