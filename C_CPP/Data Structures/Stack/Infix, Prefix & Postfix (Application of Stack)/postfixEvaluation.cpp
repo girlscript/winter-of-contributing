820c5caf76b25f618bdd3c81867d90146234608b
Pranay Sanjule <67793499+PranaySanjule@users.noreply.github.com>
2021-09-27 09:27:59 +0530

    Create postfixEvaluation.cpp

---
+++ C_CPP/Data Structures/Stack/Infix, Prefix & Postfix (Application of Stack)/postfixEvaluation.cpp
@@ -0,0 +1,65 @@
+// Program to evaluate postfix expression.
+#include<bits/stdc++.h>
+using namespace std;
+
+class postfixEvaluation
+{
+
+public:
+
+void postEval(string exp)
+{
+    stack<int> st;                              //st stack to store the ans and perform the all operations.
+    for (int i = 0; i < exp.length(); i++)
+    {
+        if (exp[i] >= '0' and exp[i] <= '9')
+        {
+            st.push(exp[i] - '0');
+        }else if(exp[i]==' '){
+            continue;
+        }
+        else
+        {
+            int second_op = st.top();
+            st.pop();
+            int first_op = st.top();
+            st.pop();
+
+            switch (exp[i])
+            {
+            case '+':
+                st.push(first_op + second_op);
+                break;
+            case '-':
+                st.push(first_op - second_op);
+                break;
+            case '*':
+                st.push(first_op * second_op);
+                break;
+            case '/':
+                st.push(first_op / second_op);
+                break;
+            case '^':
+                st.push(pow(first_op,second_op));
+                break;
+            default:
+                exit(0);
+                break;
+            }
+        }
+    }
+    cout<<st.top();
+}
+};
+
+int main()
+{
+    postfixEvaluation obj;
+
+    string exp;                                           // Time Complexity => O(l), l = length of the string
+    cout<<"Enter the postfix expression : "<<endl;
+    getline(cin, exp);
+    obj.postEval(exp);
+
+    return 0;
+}
