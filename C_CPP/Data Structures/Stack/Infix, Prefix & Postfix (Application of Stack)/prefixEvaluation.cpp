601b2c9baa330e633564c28479270c5bef0f044c
Pranay Sanjule <67793499+PranaySanjule@users.noreply.github.com>
2021-09-28 16:24:35 +0530

    Update prefixEvaluation.cpp

--- C_CPP/Data Structures/Stack/Infix, Prefix & Postfix (Application of Stack)/prefixEvaluation.cpp
+++ C_CPP/Data Structures/Stack/Infix, Prefix & Postfix (Application of Stack)/prefixEvaluation.cpp
@@ -9,22 +9,24 @@ public:
     void prefEval(string exp)
     {
         stack<int> st;                              //st stack to store the output and perform the all operations.
-        for (int i = exp.length() - 1; i >= 0; --i)   
+
+    
+        for (int i = exp.length() - 1; i >= 0; --i)   //In prefix, we traverse expression from the end.
         {
-            if (exp[i] >= '0' and exp[i] <= '9')
+            if (exp[i] >= '0' and exp[i] <= '9') //if element of expression containing element in between '0' and '9', it will convert that string into integer and push in stack.
             {
                 st.push(exp[i] - '0');
-            }else if(exp[i]==' '){
+            }else if(exp[i]==' '){              //if expression contain ' '(SPACE) skip it and check the next element of expression
                 continue;
             }
-            else
+            else                                //if element is operator, pop two operand from stack, then perform arithmetic expression on that two operands
             {
-                int first_op = st.top();
+                int first_op = st.top();    
                 st.pop();
                 int second_op = st.top();
                 st.pop();
 
-                switch (exp[i])
+                switch (exp[i])            //afther arithmetic operation push the result in stack
                 {
                 case '+':
                     st.push(first_op + second_op);
@@ -41,13 +43,13 @@ public:
                 case '^':
                 st.push(pow(first_op,second_op));
                 break;
-            default:
+                default:
                 exit(0);
                 break;
                 }
             }
         }
-        cout<<st.top();
+        cout<<st.top();                 //If traversal of the expression will complete then print the top most element of the stack.
     }
 };
 
