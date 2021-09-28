a2b32a626e8452bd986232b37f80f654c4950e35
unknown <Vandana>
2021-09-28 21:35:52 +0530

    sorting documentation edited

--- C_CPP/Algorithmic Approaches/Sorting/bubble_sort.c
+++ C_CPP/Algorithmic Approaches/Sorting/bubble_sort.c
@@ -2,17 +2,21 @@
  
 int main()
 {  
-   // array is being initialized
+   // Array is being initialized
+   // N is the total number of elements present in the array
+   // ++i operator will first execute and then increament the value.
    int arr[] = {2,9,6,5,6,0,-1}, n = 7;
    int i , j ;
    for(i = 0; i < n - 1; ++i)
    { 
       int swapped = 0;
-      // this will perform the sorting procedure
-      // here ++j will first increament the value and then pass it 
+      // This will perform the sorting procedure
+      // Here ++j will first increament the value and then pass it 
+      // The loop will iterate upto n-1 length because the indexing of elements starts from 0 to n and from backwards it will start from -1
       for(j = 0; j < (n - i - 1); ++j)
          if(arr[j] > arr[j+1])
          {
+            // For an instance , temp will store the array and then swapped will be stored.
             int temp = arr[j];
             arr[j] = arr[j+1];
             arr[j+1] = temp;
@@ -20,14 +24,17 @@ int main()
             swapped = 1;
          }
       // if array is not swapped then it will return to the loop again due to break statement.  
+      // !swapped means not swapped . NOT operator used here.
       if(!swapped)
          break;
    }
-   // this line of code will print the sorted array      
+   // this line of code will print the sorted array
+   //      
    printf("\nArray after sorting: ");
    for(i = 0; i < n; ++i)
    
    //arr[i] will print the index value of arr according to the value of i
+   // For example arr[2] will print the 3rd element of the array.
       printf("%d ", arr[i]);
  
    return 0;
--- C_CPP/Algorithmic Approaches/Sorting/insertion_sort.c
+++ C_CPP/Algorithmic Approaches/Sorting/insertion_sort.c
@@ -3,8 +3,8 @@ int main(){
 
    // Here i & j for loop counters, temp for swapping,
    // count for total number of elements, number[] to
-   // store the input numbers in array. You can increase
-   // or decrease the size of number array as per requirement
+   // Store the input numbers in array. You can increase
+   // Or decrease the size of number array as per requirement
     
    int i, j, count, temp, number[20];
 
@@ -14,7 +14,7 @@ int main(){
    printf("Enter %d elements: ", count);
 
    // This loop would store the input numbers in array
-   // here i will first implement and then increament by i++ then pass it over
+   // Here i will first implement and then increament by i++ then pass it over 
    for(i=0;i<count;i++)
       scanf("%d",&number[i]);
 
@@ -22,14 +22,15 @@ int main(){
    for(i=1;i<count;i++){
       temp=number[i];
       j=i-1;
-      //by using && operator it will work until the condition is true,it will terminate if one of the comparision become false.
+      // By using && operator it will work until the condition is true,it will terminate if one of the comparision become false.
+      
       while((temp<number[j])&&(j>=0)){
          number[j+1]=number[j];
          j=j-1;
       }
       number[j+1]=temp;
    }
-   //this line of code will the sorted array
+   // This line of code will the sorted array
    printf("elements after sorting: ");
    for(i=0;i<count;i++)
       printf(" %d",number[i]);
