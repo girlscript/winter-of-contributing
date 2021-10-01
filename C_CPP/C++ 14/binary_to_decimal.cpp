bb11844ad7ed20ceb87398d00e822484f57a7b48
Manya Sachdeva <72715296+Manya1609@users.noreply.github.com>
2021-10-01 19:34:37 +0530

    Update binary_to_decimal.cpp

--- C_CPP/C++ 14/binary_to_decimal.cpp
+++ C_CPP/C++ 14/binary_to_decimal.cpp
@@ -1,15 +1,17 @@
 /* 
+Binary Number System is a number system which uses only two digits 0 and 1.
+Decimal Number System uses 10 digits from 0 to 9 to perform all the operations.
 Example : 101101 in binary can be converted to decimal as 
 101101 = 1*(2^0) + 0*(2^1) + 1*(2^2) + 1*(2^3) + 0*(2^4) + 1*(2^5)
        = 45
    :: 101101 = 45
 */
-
+ //To convert a number from Binary to Decimal Number System
 #include<iostream>
 using namespace std;
 
 int BinaryToDecimal(int n)
-{ int ans = 0;
+{ int ans = 0;       // to store the final answer generated from the conversion of Binary to Decimal
   int x = 1;
 
   while(n > 0)
