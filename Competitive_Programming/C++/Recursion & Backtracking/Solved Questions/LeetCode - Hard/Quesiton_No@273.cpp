8cc2c5d633e0d8e132009457dfbd93e31fc491fc
Vidit <65944284+Vidit-Ostwal@users.noreply.github.com>
2021-10-02 09:13:46 +0530

    Update and rename Quesiton_No@273.md to Quesiton_No@273.cpp

--- Competitive_Programming/C++/Recursion & Backtracking/Solved Questions/LeetCode - Hard/Quesiton_No@273.cpp
+++ Competitive_Programming/C++/Recursion & Backtracking/Solved Questions/LeetCode - Hard/Quesiton_No@273.cpp
@@ -1,13 +1,9 @@
-
-
-[Leetcode question](https://leetcode.com/problems/integer-to-english-words)
-```
+/*
 The code is written in C++
 Difficulty : Hard
 Type :Recursion
-```
 
-```
+
 Explanation:
 Four different vectors have been made, named 'singles','tens','hundreds','Billion'
 singles take care of all the digitd when they come to ones places
@@ -18,28 +14,23 @@ Billion vector represent the status of the three set number.
 The 'print' function basically converts the whole vecotr into a string and reutrn that string.
 The 'Helper' function is the main function which is call repeatedly, returns the string if the number is less than 1000 or recursively 
 call itself, while increasing the 'Index' ,which defines the degree of the number, ('Index' represents the index of the Billion vector).
-  
-  ```
-  
-  
-  
- ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-```
+*/
+
+
 class Solution{
 
-
-
 public:
-
-
+/* differnt required vectors initialized */
  vector <string> singles{"Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"};
  vector <string> tens{"And","Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen"};
  vector <string> hundreds{"Zero","Ten","Twenty","Thirty","Forty","Fifty","Sixty","Seventy","Eighty","Ninety"};
  vector <string> Billion{"Hundred","Thousand","Million","Billion"};  
   
+ /* Index variable declared globally*/
   int Index;
   
+  /* Starting of function print */
   string print(vector <string> vect)
   {
     string Answer;
@@ -54,8 +45,10 @@ public:
     return Answer;
   }
   
+  /*Starting of Helper function */
   string Helper (int num)
   {
+    /*The helper function is made to given correct English word, to a max of three digit this is called recursively to get all the English Words of a number >3 digits */
     vector <string> answer;
     if (num < 1000)
     {
@@ -92,6 +85,11 @@ public:
       }
     }
 
+/*
+divido get the number which excludes all the first three digits
+modulo gets all the first three digits number,
+Both modulo and divido are recursively called by helper function, and the helper function gives a string when a three digits or less than three digit number is given. 
+*/
     int divido = num/pow(1000,Index);
     int modulo = num%int(pow(1000,Index));
     if (modulo == 0)
@@ -108,4 +106,3 @@ public:
       return Helper(num);
     }
 };
-```
