a05ea7c8342bf86c43161afeb6e19fadb9c6aec5
Subhadeep Mandal <ultrasubha@gmail.com>
2021-10-06 16:52:08 +0530

    Create hellosekai.go

---
+++ Golang/1 Introduction/hellosekai.go
@@ -0,0 +1,11 @@
+//Created By Subhadeep Mandal for Hacktoberfest
+package main
+
+import "fmt"
+
+//Declaring variable in Go to print "Hello Sekai"
+var str = "Hello, 世界"
+
+func main() {
+	fmt.Println(str)
+}
