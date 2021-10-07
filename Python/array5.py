8a23d96b0b5427febd53195c68e66f23b473bbdc
Yamini Vijaywargiya <59636994+y2010-ops@users.noreply.github.com>
2021-10-07 11:48:22 +0530

    Add files via upload

---
+++ Python/array5.py
@@ -0,0 +1,32 @@
+
+x=['indore','vidhisha','bhopal','pune']
+for i,e in enumerate(x):
+     print(i,e)
+
+x=[1,8,3]*5
+print(x)
+
+x=list(range(2,101,2))
+print(x)
+
+x=[1122,'ram',34.5]
+a,b,c=x
+print(a)
+print(b)
+print(c)
+
+x=(56,34)
+print(x)
+print(type(x))
+s=sum(x)
+print(s)
+
+x=[34,55,67]
+y=['one','two','three']
+z=list(zip(x,y))
+print(z)
+
+q=set(x)
+print(q)
+print(type(q))
+
