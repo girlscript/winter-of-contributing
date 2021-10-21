f0e03ddd090947948e3f46227fecc04b3491d43b
Nandani-koli <70843941+Nandani-koli@users.noreply.github.com>
2021-10-21 23:05:44 +0530

    Update palindrom.cpp

--- C_CPP/Competitive Programming/Patterns/palindrom.cpp
+++ C_CPP/Competitive Programming/Patterns/palindrom.cpp
@@ -1,3 +1,11 @@
+/*
+        1 
+      2 1 2 
+    3 2 1 2 3 
+  4 3 2 1 2 3 4 
+5 4 3 2 1 2 3 4 5 
+*/
+
 #include <iostream>
 using namespace std;
 int main(){
@@ -30,3 +38,4 @@ int main(){
      }
       
 }
+
