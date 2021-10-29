50198e40234a77a1ce85fa011f2a40a4b8cb605b
Nandini Kapoor <nandinikapoor24601@gmail.com>
2021-10-29 21:07:22 +0530

    add Knapsack problems in DP

---
+++ Competitive_Programming/C++/DP/Distribution_Problems/KnapSackType2.cpp
@@ -0,0 +1,51 @@
+//--// There are two friends and there are N packets where i_th packet contains A_i amount of chocolates.
+// Each packet should be given to either 1 or 2.
+// Find the minimum difference of chocolate between two.friends
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define _z ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define int long long int
+#define endl "\n"
+#define mod 1000000007
+#define pb_ push_back
+#define mp_ make_pair
+
+void solve() {
+    int n, su=0, ans=mod;
+    cin>>n;
+    int a[n];
+    for(int i=0; i<n; i++) {
+        cin>>a[i];
+        su+=a[i];
+    }
+    int mat[su+1][n+1];
+    for(int i=0; i<su+1; i++) {
+        for(int j=0; j<=n; j++) {
+            mat[i][j]=0;
+        }
+    }
+    mat[0][0]=1;
+    for(int i=0; i<=su; i++) {
+        for(int j=0; j<=n; j++) {
+            if(mat[i][j]) {
+                int temp=su-i;
+                ans=min(ans, abs(temp-i));
+                if(i+a[j]<=su && j<n) mat[i+a[j]][j+1]=1;
+                if(j<n) mat[i][j+1]=1;
+            }
+        }
+    }
+    cout<<ans<<endl;
+}
+
+int32_t main()
+{
+    _z; //Fast io
+    int t=1; //Initialize number of test cases as 1
+    //cin>>t;  // Use in case of multiple test cases
+    while(t--) {
+        solve(); //Call utility function for each test case
+    }
+}
---
+++ Competitive_Programming/C++/DP/Distribution_Problems/KnapsackType1.cpp
@@ -0,0 +1,79 @@
+//--// There are N people and there is shop where there are C candies. An i_th person will but C_i 
+//chocolates and will give R_i money to shopkeeper.
+// There are some friends. You are given M pairs of friends where (x,y) means x and y are friends.
+// They are weird and if the person P buys candies then none of his friends 
+//(direct/indirect) friends should not buy candies.
+// Maximize the amount of money shopkeeper could earn
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define _z ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define int long long int
+#define endl "\n"
+#define mod 1000000007
+#define pb_ push_back
+#define mp_ make_pair
+
+void dfs(vector<int>g[], int n, int cur, int vis[], vector<int>&se) {
+    vis[cur]=1;
+    se.pb_(cur);
+    for(auto it:g[cur]) {
+        if(!vis[cur]) dfs(g, n, it, vis, se);
+    }
+}
+
+void solve() {
+    int c, n;
+    cin>>c>>n;
+    int can[n], pri[n], visi[n]={0};
+    for(int i=0; i<n; i++) cin>>can[i];
+    for(int i=0; i<n; i++) cin>>pri[i];
+    int m;
+    cin>>m;
+    vector<int>graph[n];
+    for(int i=0; i<m; i++) {
+        int temp1, temp2;
+        cin>>temp1>>temp2;
+        temp1--;
+        temp2--;
+        graph[temp1].pb_(temp2);
+        graph[temp2].pb_(temp1);
+    }
+    vector<vector<int>>sets;
+    for(int i=0; i<n; i++) {
+        if(!visi[i]) {
+            vector<int>se;
+            dfs(graph, n, i, visi, se);
+            sets.pb_(se);
+        }
+    }
+    int no=sets.size(), mat[no+1][c+1];
+    for(int i=0; i<no+1; i++) {
+        for(int j=0; j<c+1; j++) mat[i][j]=0;
+    }
+    for(int i=1; i<no+1; i++) {
+        for(int j=1; j<c+1; j++) {
+            for(int k=0; k<sets[i-1].size(); k++) {
+                int x=sets[i-1][k];
+                if(can[x]<=j) {
+                    mat[i][j]=max(max(mat[i][j], mat[i-1][j]), mat[i-1][j-can[x]]+pri[x]);
+                }
+                else {
+                    mat[i][j]=max(mat[i][j], mat[i-1][j]);
+                }
+            }
+        }
+    }
+    cout<<mat[no][c]<<endl;
+}
+
+int32_t main()
+{
+    _z; //Fast io
+    int t=1; //Initialize number of test cases as 1
+    //cin>>t;  // Use in case of multiple test cases
+    while(t--) {
+        solve(); //Call utility function for each test case
+    }
+}
