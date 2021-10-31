422cec02142c27527c7badeb1422ddd2082b09cd
Chahak02 <83837855+Chahak02@users.noreply.github.com>
2021-10-30 23:16:32 -0700

    Update Infinitearray.java

--- Competitive_Programming/Java/Array/infinite array/Infinitearray.java
+++ Competitive_Programming/Java/Array/infinite array/Infinitearray.java
@@ -1,4 +1,8 @@
-
+// Searching in infinite array .Basically the approach is first we willbe taking two elements checking whether target is less than end element if greater than then we will double the array
+//the array start will move to end and end will move to start+(end-start)/2 and so on when we will find the start and end index we will pass it for binary search 
+//Both the time we are doinf binary search in first function we are moving opposite of binary search search which means in binary search we half the arrays but here i am double the array to find my search start and end
+// in second function we are doing normal search to find the target elemnt position .
+// Throughout the program I have not used length function since it is infinite array so we do not know the length of the array.
 import java.util.*;
 
 public class Infinitearray {
@@ -12,21 +16,21 @@ public class Infinitearray {
 		System.out.println(ifinitearray(a,target));
 
 	}
-static int ifinitearray(int[] a,int target)
+static int ifinitearray(int[] a,int target) //This function is finding the start and end for binary search
 {
 	int start=0;
 	int end=1;
 	while(target>a[end]) {
 		
 		int temp=end+1;
-		end=end+(end-start+1)*2;
+		end=end+(end-start+1)*2;   // doubling the array
 		start=temp;
 	}
 
 	return binsrch(a,start,end,target);
 		
 }
-static int binsrch(int[] a,int start,int end,int target)
+static int binsrch(int[] a,int start,int end,int target)   // normal binary search to find the index of our target element
 {
 	while(start<=end)
 	{
