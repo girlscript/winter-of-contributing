d3aa9b68005e4535125ee6bb7f00b9a860f9df57
Chahak02 <83837855+Chahak02@users.noreply.github.com>
2021-11-17 22:54:22 -0800

    Update Cyclicsort.java

--- Competitive_Programming/cyclic_sort/Cyclicsort.java
+++ Competitive_Programming/cyclic_sort/Cyclicsort.java
@@ -1,3 +1,10 @@
+// It is a sorting technique when range  of numbers are given .If the number are given from 1 to n then 1 will go to 0th index that means n will go in n-1 position index.
+//Similarly if range is given from 0 to n then 0 will go to 0th index ,n will go to nth index and soon.
+//The type of question where we use the trick is find the duplicate numbers in the array given that the numbers are in range 1 to n.
+//Whenever the ques contains the word range we must always use cyclic sort first.
+//Below program defines how to sort using cyclic sort technique
+
+
 import java.util.*;
 
 class Cyclicsort{
