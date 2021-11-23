8297cffef8328971f353bcbdebacc5e3a9775ceb
navee <naveenarameshvardhini@gmail.com>
2021-11-23 11:37:08 +0530

    View Image

--- Android_Development_With_Flutter/Flutter_project/cloud_photos/lib/Screen/viewImage.dart
+++ Android_Development_With_Flutter/Flutter_project/cloud_photos/lib/Screen/viewImage.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+
 class ViewImage extends StatefulWidget {
   final String image;
   const ViewImage({Key? key, required this.image}) : super(key: key);
@@ -19,12 +20,12 @@ class _ViewImageState extends State<ViewImage> {
           onTap: () {
             Navigator.pop(context);
           },
-          child: Container(
-            height: MediaQuery.of(context).size.height,
-            width: MediaQuery.of(context).size.width,
-            child: Image.network(
-              widget.image,
-              fit: BoxFit.cover,
+          child: Center(
+            child: Container(
+              child: Image.network(
+                widget.image,
+                fit: BoxFit.cover,
+              ),
             ),
           ),
         ),
